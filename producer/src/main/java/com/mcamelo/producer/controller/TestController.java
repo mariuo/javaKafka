c83ae5b051e1f3523c3800786c262928163d87d9
Mario Camelo <mariuo@gmail.com>
2023-03-10 15:39:09 -0500

    :sparkles: fix: Project init

---
+++ producer/src/main/java/com/mcamelo/producer/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.mcamelo.producer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @GetMapping("/send")
+    public ResponseEntity<?> send(){
+        kafkaTemplate.send("topic-1", "Send de: "+ LocalDateTime.now());
+        return ResponseEntity.ok().build();
+    }
+}
