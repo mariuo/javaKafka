9b766347cd5d2eb0556c1d4022a4eacead64ac34
Mario Camelo <mariuo@gmail.com>
2023-03-13 14:04:51 -0400

    :sparkles: feet: Interceptors in config

--- producer/src/main/java/com/mcamelo/producer/controller/TestController.java
+++ producer/src/main/java/com/mcamelo/producer/controller/TestController.java
@@ -2,16 +2,15 @@ package com.mcamelo.producer.controller;
 
 import com.mcamelo.producer.model.City;
 import com.mcamelo.producer.model.Person;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
-import java.util.stream.IntStream;
-
+import java.util.Random;
+@Slf4j
 @RestController
 public class TestController {
 
@@ -46,7 +45,9 @@ public class TestController {
 
     @GetMapping("/sendPerson")
     public void sendPerson() {
-        jsonKafkaTemplate.send("person-topic", new Person("Joao", 10));
+        var person = new Person("John", (Integer)new Random().nextInt(50));
+        log.info(person.toString());
+        jsonKafkaTemplate.send("person-topic", person);
     }
     @GetMapping("/sendCity")
     public void sendCity() {
--- producer/src/main/java/com/mcamelo/producer/model/Person.java
+++ producer/src/main/java/com/mcamelo/producer/model/Person.java
@@ -1,9 +1,6 @@
 package com.mcamelo.producer.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.io.Serializable;
 
@@ -11,6 +8,7 @@ import java.io.Serializable;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class Person implements Serializable {
     private String name;
     private Integer age;
